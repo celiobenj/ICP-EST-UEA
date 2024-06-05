fa684ca4fd8efc65e63a1836db9f95507140095f
Célio Benjamim <104313743+celiobenj@users.noreply.github.com>
2024-06-05 09:06:59 -0400

    Update questão 2.py

--- Aulas/Aula 02 (05-06-24)/questão 2/questão 2.py
+++ Aulas/Aula 02 (05-06-24)/questão 2/questão 2.py
@@ -4,14 +4,16 @@ hora = float(input("Quantas horas você trabalha por mês? "))
 salBruto = ganhoHora * hora
 
 # impostos
-INSS = salBruto*(11/100)
+IR = salBruto*(11/100)
+INSS = salBruto*(8/100)
 sind = salBruto*(5/100)
 
 # salário líquido
-salLiq = salBruto - INSS - sind
+salLiq = salBruto - IR - INSS - sind
 
 print(f"""
 Seu salário bruto é de R${salBruto:,.2f}.
+Você pagou ao IR um total de R${IR:,.2f}.
 Você pagou ao INSS um total de R${INSS:,.2f}.
 Você pagou ao sind um total de R${sind:,.2f}.
 Seu salário líquido é de R${salLiq:,.2f}.
